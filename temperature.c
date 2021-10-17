498c56a5cf3d41817dc02087e9cdd9e2429b49d6
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-17 20:43:04 +0100

    Create temperature.c

---
+++ temperature.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    
+    void temperatureConvert(void){
+        char answser;
+        int temp;
+        printf("Choose A or B\nA. Celcius to Fahrenheit\nB. Fahrenheit to Celcius\nwhat is your answser: ");
+        scanf("%c", &answser);
+        if (answser == 'A'){
+            printf("\nEnter your temperature in Celcius: ");
+            scanf("%i", &temp);
+            float formula = (float) (temp*1.8) + 32;
+            printf("\n%.2f F", formula);
+        }else if(answser == 'B'){
+            printf("\nEnter your temperature in Fahrenheit: ");
+            scanf("%i", &temp);
+            float formula = (float) (temp - 32) / 1.8;
+            printf("\n%.2f C°", formula);
+        }else{
+            system("clear");
+            temperatureConvert();
+        }
+    }
+    temperatureConvert();
+    return 0;
+}
