83860e4679c1e7918c2f70140b2b802fe8cd7e28
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-12-27 10:50:40 +0100

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -5,7 +5,7 @@ from playsound import playsound
 # Get Input from the user
 name = str(input("What's your name? "))
 # Format text
-text = f"Hello, {name}!______This text to speech test____This Google translate voice over______Goodbye"
+text = f"Hello, {name}!______This is a text to speech test____This is Google translate voice over______Goodbye"
 # Turn text to speech and save
 tts = gTTS(text)
 tts.save('test.mp3')
